084d6e272f37ab0bcfed4e9448db0dffff095261
Chris Smith <chris@dmdirc.com>
2009-01-06 09:36:30 +0000

    Add missing pagecache.sql

---
+++ src/uk/co/md87/evetool/api/db/pagecache.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PageCache (
+    pc_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    pc_method VARCHAR(50),
+    pc_args VARCHAR(512),
+    pc_cachedat TIMESTAMP,
+    pc_cacheduntil TIMESTAMP,
+    pc_data CLOB
+)
